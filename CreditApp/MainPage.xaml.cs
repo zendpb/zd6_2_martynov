fc88d6e695223d1a126e61db2d7604fb09faa69c
RyzhkovAA <qwerty@bebr.ru>
2024-12-02 14:30:54 +0500

    first commit

---
+++ CreditApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CreditApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void Button_ClickedAsync(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(userLine.Text) || string.IsNullOrWhiteSpace(passLine.Text))
+            {
+                await DisplayAlert("Ошибка", "Пожалуйста, заполните все поля.", "OK");
+            }
+            else
+            {
+                string name = userLine.Text;
+                await Navigation.PushModalAsync(new NavigationPage(new TabbedPage1(name)));
+            }
+        }
+
+       
+    }
+}
---
+++ CreditApp/TabbedPage1.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CreditApp
+{
+    
+    public partial class TabbedPage1 : TabbedPage
+    {
+        public TabbedPage1(string name)
+        {
+            InitializeComponent();
+            this.FindByName<Label>("labelname").Text=name;
+            RefreshData();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderValueLabel.Text = $"{e.NewValue:F0}%"; 
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (LoanAmountEntry.Text != null && LoanTermEntry.Text != null && PaymentTypePicker.SelectedItem != null)
+            {
+                double loanAmount = double.Parse(LoanAmountEntry.Text);
+                int loanTerm = int.Parse(LoanTermEntry.Text);
+                double interestRate = Slider.Value;
+                string paymentType = PaymentTypePicker.SelectedItem.ToString();
+
+                if (loanAmount > 0 && loanTerm > 0)
+                {
+                    double monthlyPayment = 0;
+                    double totalAmount = 0;
+                    double overpayment = 0;
+
+                    if (paymentType == "Аннуитетный")
+                    {
+
+                        double monthlyRate = interestRate / 100 / 12;
+                        monthlyPayment = loanAmount * (monthlyRate * Math.Pow(1 + monthlyRate, loanTerm)) / (Math.Pow(1 + monthlyRate, loanTerm) - 1);
+                        totalAmount = monthlyPayment * loanTerm;
+                        overpayment = totalAmount - loanAmount;
+                    }
+                    else if (paymentType == "Дифференцированный")
+                    {
+
+                        double monthlyRate = interestRate / 100 / 12;
+                        double principalPayment = loanAmount / loanTerm;
+                        
+                        monthlyPayment = 0;
+
+                        totalAmount = 0;
+                        for (int i = 0; i < loanTerm; i++)
+                        {
+                            double interestPayment = (loanAmount - (principalPayment * i)) * monthlyRate;
+                            totalAmount += principalPayment + interestPayment;
+                        }
+                        overpayment = totalAmount - loanAmount;
+                    }
+
+
+                    MonthlyPaymentLabel.Text = $"{monthlyPayment:F2} руб.";
+                    TotalAmountLabel.Text = $"{totalAmount:F2} руб.";
+                    OverpaymentLabel.Text = $"{overpayment:F2} руб.";
+                }
+            }
+
+        }
+
+
+        public void RefreshData()
+        {
+            DateTime currentDate = DateTime.Now;
+            CurrentDateLabel.Text = currentDate.ToString("dd/MM/yyyy");
+        }
+
+    }
+}
